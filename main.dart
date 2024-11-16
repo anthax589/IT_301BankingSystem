d9afa1323ea38b086839219b4c64e6aa9d25f8e9
anthrax589 <jaymartuba2@gamil.com>
2024-11-16 15:07:06 +0800

    updated main.dart

--- main.dart
+++ main.dart
@@ -21,3 +21,10 @@ void main() {
     }
   }
 }  
+void clear(){
+    if (Platform.isWindows){
+        print(Process.runSync("cls", [], runInShell: true).stdout);
+    } else {
+        print(Process.runSync("clear", [], runInShell: true).stdout);
+}
+}
